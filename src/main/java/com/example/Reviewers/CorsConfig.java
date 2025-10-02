13405191c578f89739daefa7aaf31cfb0c218023
Sachingupta82 <nicrooctal16@gmail.com>
2025-10-02 23:13:44 +0530

    Cors added

--- src/main/java/com/example/Reviewers/CorsConfig.java
+++ src/main/java/com/example/Reviewers/CorsConfig.java
@@ -14,7 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Angular dev server
+                        .allowedOrigins(
+                            "http://localhost:4200",
+                            "https://reviewers-kohl.vercel.app",
+                            "https://reviewers-h6jejskat-sachingupta82s-projects.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
